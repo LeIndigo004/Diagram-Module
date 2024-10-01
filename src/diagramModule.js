37282aa902ec287993d44caa8a211fb656cd8807
Leia Lindberg <ll224np@student.lnu.se>
2024-10-01 21:20:23 +0100

    Create private fields for incapsulation and create method setSize

--- src/diagramModule.js
+++ src/diagramModule.js
@@ -4,3 +4,37 @@
  * @author Leia Lindberg <ll224np@student.lnu.se>
  * @version 1.0.0
  */
+
+/**
+ * Creates the main class for this diagram module.
+ */
+export class DiagramModule {
+  #ctx
+  #width
+  #height
+
+  constructor (canvasId) {
+    const canvas = document.getElementById(canvasId)
+    this.#ctx = canvas.getContext('2d')
+    
+    this.setSize('400', '400')
+  }
+
+  /**
+   * A public method where the user can decide the width and height of the canvas.
+   *
+   * @param {string} width - The width of the canvas.
+   * @param {string} height - The height of the canvas.
+   */
+  setSize (width, height) {
+    const canvas = this.#ctx.canvas
+    canvas.width = width
+    canvas.height = height
+  
+    // Set the values to the private fields
+    this.#width = width
+    this.#height = height
+    console.log(`Canvas size set to: ${this.#width}x${this.#height}`)
+  }
+
+}
