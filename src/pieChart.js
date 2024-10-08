973bae5005a2ed413b1585bc361f2ac4784e47eb
Leia Lindberg <ll224np@student.lnu.se>
2024-10-08 23:34:46 +0100

    Separate the labels and styles

--- src/pieChart.js
+++ src/pieChart.js
@@ -22,9 +22,8 @@ export class PieChart {
    * @param {string[]} colors - The given colors
    */
     drawChart (data) {
-      const label = data.map(item => item.label)
       const value = data.map(item => item.value)
-      const color = data.map(item => item.color)
+
       // Get the percent of each value
       const total = value.reduce((sum, value) => sum + value, 0)
       let startAngle = 0
@@ -45,21 +44,25 @@ export class PieChart {
         this.#ctx.moveTo(centralX, centralY)
         this.#ctx.arc(centralX, centralY, radius, startAngle, endAngle)
         this.#ctx.closePath()
+
+        this.#setLabels(i, data, total)
   
-        // The labels
+        startAngle = endAngle
+      }
+    }
+
+    #setLabels (index, data, total) {
+        const textPosition = this.#height * 0.15 + (index * (this.#height * 0.07))
         this.#ctx.font = `bold ${this.#height * 0.025}px Lucida Console`
         this.#ctx.textAlign = 'left'
         this.#ctx.fillStyle = 'black'
-        this.#ctx.fillText(`${label[i].charAt(0).toUpperCase() + label[i].slice(1)}: ${
-        (value[i]/ total * 100).toFixed(2)}%`, 
+        this.#ctx.fillText(`${data[index].label.charAt(0).toUpperCase() + data[index].label.slice(1)}: ${
+        (data[index].value/ total * 100).toFixed(2)}%`, 
         this.#width * 0.05, textPosition)
   
         // Small dots beside labels
         this.#ctx.arc(this.#width * 0.02, textPosition * 0.96, 8, 0, 2 * Math.PI)
-        this.#ctx.fillStyle = color[i]
+        this.#ctx.fillStyle = data[index].color
         this.#ctx.fill()
-  
-        startAngle = endAngle
-      }
     }
 }
