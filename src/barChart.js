0df7eb9fcb45397bb381fba36ab33f02581aec5e
Leia Lindberg <ll224np@student.lnu.se>
2024-11-18 01:57:38 +0000

    Fix the code quality

--- src/barChart.js
+++ src/barChart.js
@@ -14,7 +14,8 @@ export class BarChart {
   #marginHeight
   #marginWidth
   #axes
-  constructor (ctx, width, height) {
+
+  constructor(ctx, width, height) {
     this.#ctx = ctx
     this.#width = width
     this.#height = height
@@ -22,46 +23,66 @@ export class BarChart {
   }
 
   /**
- * Creates a bar chart with customizable labels for y and x axels, and colors.
- *
- * @param {Object[]} data - The given data to track in the diagram
- * @param {String} xTitle - The title of the x axel
- * @param {String} yTitle - The title of the y axel
- * @param {Number} maxValueForY - The highest value on the y axel
- * @param {Number} numOfYLabels - The amount of written out labels you want on the y axel
- */
-  drawChart (data, yTitle, xTitle, maxValueForY, numOfYLabels) {
-    const label = data.map(item => item.label)
-    const value = data.map(item => item.value)
-    const color = data.map(item => item.color)
+   * Renders the bar chart with axes and bars.
+   *
+   * @param {Object[]} data - Array of objects containing `label`, `value`, and `color` for each bar.
+   * @param {String} yTitle - Title for the y-axis.
+   * @param {String} xTitle - Title for the x-axis.
+   * @param {Number} maxValueForY - Maximum value for the y-axis.
+   * @param {Number} numOfYLabels - Number of y-axis labels.
+   */
+  drawChart(data, yTitle, xTitle, maxValueForY, numOfYLabels) {
+    this.#calculateMargins()
+    this.#axes.drawLabels(yTitle, xTitle, this.#extractLabels(data), maxValueForY, numOfYLabels, false)
 
-    // Define margins for bar chart
+    const barWidth = this.#calculateBarWidth(data.length)
+    data.forEach((item, index) => this.#drawBar(item, index, barWidth, maxValueForY, data.length))
+  }
+
+  #calculateMargins() {
     this.#marginHeight = this.#height * 0.2
     this.#marginWidth = this.#width * 0.2
+  }
 
-    // Get the axes and its labels
-    this.#axes.drawLabels(yTitle, xTitle, label, maxValueForY, numOfYLabels, false)
-    const availableWidth = this.#width - this.#marginWidth * 2
-    const barWidth = availableWidth / label.length * 0.5 // Adjust the bar width 
+  #extractLabels(data) {
+    return data.map(item => item.label)
+  }
 
-    for (let i = 0; i < data.length; i++) {
-      // Validate input
-      if (maxValueForY < value[i]) {
-        throw new Error('The max value for Y cannot be smaller than the given values')
-      }
-       // Calculate the central position of each label on x
-       const labelX = this.#marginWidth + (i + 0.5) * (availableWidth / label.length)
-      
-       // Get the bar height
-       const barHeight = ((value[i])/ maxValueForY) * (this.#height - 2 * this.#marginHeight)
- 
-       // Get the bar on the right position through dividing the width of the
-       // bar by 2 and substract it with the central position of each label
-       const barPosition = labelX - barWidth / 2
-       this.#ctx.beginPath()
-       this.#ctx.fillStyle = color[i]
-       this.#ctx.fillRect(barPosition, this.#height - this.#marginHeight, barWidth, -barHeight)
-       this.#ctx.stroke()
+  #calculateBarWidth(numBars) {
+    const availableWidth = this.#width - 2 * this.#marginWidth
+    return (availableWidth / numBars) * 0.5 // Adjust bar width as a fraction of available space
+  }
+
+  #drawBar(item, index, barWidth, maxValueForY, numBars) {
+    if (item.value > maxValueForY) {
+      throw new Error('The max value for Y cannot be smaller than the given values')
     }
+
+    const labelX = this.#calculateLabelPosition(index, numBars)
+    const barHeight = this.#calculateBarHeight(item.value, maxValueForY)
+    const barPosition = this.#calculateBarPosition(labelX, barWidth)
+
+    this.#renderBar(barPosition, barHeight, barWidth, item.color)
+  }
+
+  #calculateLabelPosition(index, numBars) {
+    const availableWidth = this.#width - 2 * this.#marginWidth
+    return this.#marginWidth + (index + 0.5) * (availableWidth / numBars)
+  }
+
+  #calculateBarHeight(value, maxValueForY) {
+    const chartHeight = this.#height - 2 * this.#marginHeight
+    return (value / maxValueForY) * chartHeight
+  }
+
+  #calculateBarPosition(labelX, barWidth) {
+    return labelX - barWidth / 2
+  }
+
+  #renderBar(barPosition, barHeight, barWidth, color) {
+    this.#ctx.beginPath()
+    this.#ctx.fillStyle = color
+    this.#ctx.fillRect(barPosition, this.#height - this.#marginHeight, barWidth, -barHeight)
+    this.#ctx.stroke()
   }
 }
